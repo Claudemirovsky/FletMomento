a377816d419d22120315e31341f9f8641f225fa6
Vinicius Castro <150952242+Vinicius-de-Castro@users.noreply.github.com>
2025-01-08 23:12:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import flet as ft
 import time
 
+
 class Criar(ft.Button):
     def __init__(self, on_click):
         super().__init__()
-        self.text="Criar horário"
-        self.on_click=on_click
+        self.text = "Criar horário"
+        self.on_click = on_click
+
 
 class Horario(ft.Column):
-    def __init__(self, cancel, confirm):
+    def __init__(self, cancel, confirm, chooseTime):
         super().__init__()
         self.controls = [
             ft.Text(value="Adicionar horário", color=ft.Colors.BLACK, size=20),
@@ -18,15 +20,7 @@ class Horario(ft.Column):
             ]),
             ft.Row([
                 ft.Text(value="Horário:", width=60, color=ft.Colors.BLACK),
-                ft.Dropdown(
-                    options=[ft.dropdown.Option(x) for x in range(1,13)],
-                    width=50,
-                    color=ft.Colors.BLACK),
-                ft.Text(value = ":", color=ft.Colors.BLACK),
-                ft.Dropdown(
-                    options=[ft.dropdown.Option(x) for x in range(60)],
-                    width=50,
-                    color=ft.colors.BLACK)
+                ft.ElevatedButton(value="00:00", on_click=chooseTime)
             ]),
             ft.Row([
                 ft.Button(text="Cancelar", on_click=cancel),
@@ -40,7 +34,7 @@ def main(page: ft.Page):
     def scheduleCreate(horario: "Horario"):
         page.remove_at(-1)
         page.add(ft.Container(
-            content=Horario(cancel, confirm),
+            content=Horario(cancel, confirm, chooseTime),
             bgcolor=ft.Colors.WHITE,
             alignment=ft.alignment.center,
             margin=10,
@@ -49,14 +43,28 @@ def main(page: ft.Page):
             height=250,
             border_radius=16)
         )
-    
+
+    def timeChosen(e):
+        print({time_picker.value})
+
+    def chooseTime(e):
+        page.open(time_picker)
+
     def cancel(criar: "Criar"):
         page.remove_at(-1)
         page.add(Criar(on_click=scheduleCreate))
 
-    def confirm(e):
+    def confirm(criar: "Criar"):
         pass
 
+    time_picker = ft.TimePicker(
+        confirm_text="Confirmar",
+        error_invalid_text="Horário inválido",
+        help_text="Escolha um horário carniça",
+        on_change=timeChosen
+    )
+
     page.add(Criar(on_click=scheduleCreate))
+
 
 ft.app(main)
