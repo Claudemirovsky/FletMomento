f7347c7383278474106b550aac974a6ff89fe78f
Vinicius Castro <150952242+Vinicius-de-Castro@users.noreply.github.com>
2025-01-15 20:54:49 -0300

    Vai se fuder Claudemir

--- main.py
+++ main.py
@@ -1,25 +1,47 @@
 import flet as ft
+import random
 
-class Horario(ft.Container):
+
+class Horario(ft.Card):
     def __init__(self, color, name, start, end):
         super().__init__()
-        self.width=60,
-        self.height=60,
-        self.border_radius=20
-        self.bgcolor=color
-        self.content=ft.Row(
-            [
-                ft.Text(value=name, color="#F5FFF5", size=12, weight=ft.FontWeight.BOLD),
-                ft.Text(f"{start}-{end}")
-            ]
+        self.margin = 0
+        self.content = ft.Container(
+            border_radius=10,
+            bgcolor=color,
+            padding=10,
+            content=ft.Column(
+                [
+                    ft.Text(value=name, color="#F5FFF5",
+                            size=10, weight=ft.FontWeight.BOLD),
+                    ft.Text(f"{start}-{end}", color="#F5FFF5",
+                            size=7, weight=ft.FontWeight.BOLD)
+                ],
+                width=45,
+                height=60,
+                alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            )
         )
+
+
+def horarioAleatorio(dia):
+    cores = ["#FBBC4D", "#42CA62", "#F54A4A"]
+    cadeiras = ["Cálculo II", "Lógica Matemá.", "Libras", "Mat. Discreta",
+                "Ética e Filosofia", "Pro. Orie. a Objetos", "Arq. de Compu.", "Banco de Dados"]
+    dia = [ft.Text(value=dia, color="#F5FFF5",
+                   size=30, weight=ft.FontWeight.BOLD, text_align=ft.TextAlign.CENTER)]
+    for i in range(12, random.randrange(13, 18)):
+        dia.append(Horario(color=random.choice(cores), name=random.choice(
+            cadeiras), start=f"{i}:00", end=f"{i+1}:00"))
+    return dia
+
 
 def main(page: ft.Page):
 
-    YELLOW="#FEC562"
-    BLUE="#4A92FF"
-    GREY="#F5FFF5"
-    DARK_BLUE="#0B2E4F"
+    YELLOW = "#FEC562"
+    BLUE = "#4A92FF"
+    GREY = "#F5FFF5"
+    DARK_BLUE = "#0B2E4F"
 
     login = ft.View(
         route="login",
@@ -29,7 +51,8 @@ def main(page: ft.Page):
             gradient=ft.LinearGradient(
                 begin=ft.alignment.top_center,
                 end=ft.alignment.bottom_center,
-                colors=[BLUE, DARK_BLUE]
+                colors=[BLUE, "#2C5899"],
+                stops=[0.0, 0.86]
             )
         ),
         controls=[
@@ -38,73 +61,83 @@ def main(page: ft.Page):
                     content=ft.Container(
                         content=ft.Column(
                             [
-                                #Titulo
+                                # Titulo
                                 ft.Column(
                                     [
                                         ft.Row(
                                             [
-                                                ft.Text(value="TIME", color=BLUE, weight=ft.FontWeight.BOLD, size=40),
-                                                ft.Text(value= "WISE", color=YELLOW,
-                                                weight=ft.FontWeight.BOLD, size=40)
+                                                ft.Text(
+                                                    value="TIME", color=BLUE, weight=ft.FontWeight.BOLD, size=40),
+                                                ft.Text(value="WISE", color=YELLOW,
+                                                        weight=ft.FontWeight.BOLD, size=40)
                                             ],
                                             spacing=0,
                                             alignment=ft.MainAxisAlignment.CENTER
                                         ),
-                                        ft.Text("Organize, estude, conquiste!", weight=ft.FontWeight.BOLD, size=16, color=DARK_BLUE)
+                                        ft.Text(
+                                            "Organize, estude, conquiste!", weight=ft.FontWeight.BOLD, size=16, color=DARK_BLUE)
                                     ],
                                     spacing=0,
                                     horizontal_alignment=ft.CrossAxisAlignment.CENTER
                                 ),
 
-                                #Email
+                                # Email
                                 ft.Column(
                                     [
-                                        ft.Text(value=" Email", color=DARK_BLUE, weight=ft.FontWeight.BOLD, size=16),
+                                        ft.Text(value=" Email", color=DARK_BLUE,
+                                                weight=ft.FontWeight.BOLD, size=16),
                                         ft.TextField(
                                             color=BLUE,
-                                            hint_text="seuemail@email.com.br", 
-                                            width=330, 
-                                            border_color=BLUE, 
-                                            border_radius=10, 
+                                            hint_text="seuemail@email.com.br",
+                                            width=330,
+                                            border_color=BLUE,
+                                            border_radius=10,
                                             hint_style=ft.TextStyle(color=BLUE))
                                     ],
                                     spacing=4
                                 ),
 
-                                #Senha
+                                # Senha
                                 ft.Column(
                                     [
-                                        ft.Text(value=" Senha", color=DARK_BLUE, weight=ft.FontWeight.BOLD, size=16,),
+                                        ft.Text(value=" Senha", color=DARK_BLUE,
+                                                weight=ft.FontWeight.BOLD, size=16,),
                                         ft.TextField(
                                             color=BLUE,
-                                            hint_text="•••••••", 
-                                            width=330, 
-                                            border_color=BLUE, 
-                                            border_radius=10, 
-                                            hint_style=ft.TextStyle(color=BLUE),
+                                            hint_text="•••••••",
+                                            width=330,
+                                            border_color=BLUE,
+                                            border_radius=10,
+                                            hint_style=ft.TextStyle(
+                                                color=BLUE),
                                             password=True)
                                     ],
                                     spacing=4
                                 ),
 
-                                #Lembre-se
+                                # Lembre-se
                                 ft.Row(
                                     [
-                                        ft.Checkbox(label="Lembre-se de mim", label_style=ft.TextStyle(color=DARK_BLUE, size=12)),
-                                        ft.TextButton(content=ft.Text(value="Esqueceu sua senha?", text_align=ft.TextAlign.RIGHT, style=ft.TextStyle(decoration=ft.TextDecoration.UNDERLINE, size=12), color=DARK_BLUE))
+                                        ft.Checkbox(
+                                            label="Lembre-se de mim", label_style=ft.TextStyle(color=DARK_BLUE, size=12)),
+                                        ft.TextButton(content=ft.Text(value="Esqueceu sua senha?", text_align=ft.TextAlign.RIGHT, style=ft.TextStyle(
+                                            decoration=ft.TextDecoration.UNDERLINE, size=12), color=DARK_BLUE))
                                     ],
                                     width=330,
                                     alignment=ft.MainAxisAlignment.SPACE_BETWEEN
                                 ),
 
-                                #Entrar
-                                ft.ElevatedButton(content=ft.Text(value="ENTRAR", color=ft.Colors.WHITE, size=16, weight=ft.FontWeight.BOLD), bgcolor=YELLOW, width=330, height=48, on_click=lambda _: page.go("main")),
+                                # Entrar
+                                ft.ElevatedButton(content=ft.Text(value="ENTRAR", color=ft.Colors.WHITE, size=16, weight=ft.FontWeight.BOLD),
+                                                  bgcolor=YELLOW, width=330, height=48, on_click=lambda _: page.go("main")),
 
-                                #Não tem conta ainda
+                                # Não tem conta ainda
                                 ft.Row(
                                     [
-                                        ft.Text(value="Não tem conta?", color=DARK_BLUE, weight=ft.FontWeight.BOLD, size=16),
-                                        ft.TextButton(content=ft.Text(value="Crie agora", text_align=ft.TextAlign.RIGHT, style=ft.TextStyle(decoration=ft.TextDecoration.UNDERLINE, size=16), color=BLUE))
+                                        ft.Text(
+                                            value="Não tem conta?", color=DARK_BLUE, weight=ft.FontWeight.BOLD, size=16),
+                                        ft.TextButton(content=ft.Text(value="Crie agora", text_align=ft.TextAlign.RIGHT, style=ft.TextStyle(
+                                            decoration=ft.TextDecoration.UNDERLINE, size=16), color=BLUE))
                                     ],
                                     spacing=0,
                                     alignment=ft.MainAxisAlignment.CENTER
@@ -112,14 +145,16 @@ def main(page: ft.Page):
 
                                 ft.Divider(height=10, color=DARK_BLUE),
 
-                                #Ou entre com
-                                ft.Text(value="Ou entre com", color=DARK_BLUE, size=20, weight=ft.FontWeight.BOLD),
+                                # Ou entre com
+                                ft.Text(value="Ou entre com", color=DARK_BLUE,
+                                        size=20, weight=ft.FontWeight.BOLD),
 
-                                #Icones
+                                # Icones
                                 ft.Row(
                                     [
                                         ft.Container(
-                                            content=ft.Text("Facebook"),
+                                            content=ft.Image(
+                                                src=f"social.png", fit=ft.ImageFit.COVER, color_blend_mode=ft.BlendMode.EXCLUSION),
                                             margin=10,
                                             padding=10,
                                             bgcolor=BLUE,
@@ -128,7 +163,8 @@ def main(page: ft.Page):
                                             border_radius=16
                                         ),
                                         ft.Container(
-                                            content=ft.Text("Instagram"),
+                                            content=ft.Image(
+                                                src=f"instagram.png", fit=ft.ImageFit.COVER, color_blend_mode=ft.BlendMode.EXCLUSION),
                                             margin=10,
                                             padding=10,
                                             bgcolor=BLUE,
@@ -137,7 +173,8 @@ def main(page: ft.Page):
                                             border_radius=16
                                         ),
                                         ft.Container(
-                                            content=ft.Text("Twitter"),
+                                            content=ft.Image(
+                                                src=f"twitter.png", fit=ft.ImageFit.COVER, color_blend_mode=ft.BlendMode.EXCLUSION),
                                             margin=10,
                                             padding=10,
                                             bgcolor=BLUE,
@@ -145,7 +182,7 @@ def main(page: ft.Page):
                                             height=80,
                                             border_radius=16
                                         )
-                                        
+
                                     ],
                                     alignment=ft.MainAxisAlignment.SPACE_AROUND
                                 )
@@ -172,7 +209,7 @@ def main(page: ft.Page):
             gradient=ft.LinearGradient(
                 begin=ft.alignment.top_center,
                 end=ft.alignment.bottom_center,
-                colors=[BLUE, DARK_BLUE]
+                colors=[BLUE, "#2C5899"]
             )
         ),
         controls=[
@@ -182,20 +219,48 @@ def main(page: ft.Page):
                     content=ft.Row(
                         [
                             ft.Row(
-                                    [
-                                        ft.Text(value="TIME", color=BLUE, weight=ft.FontWeight.BOLD, size=30),
-                                        ft.Text(value= "WISE", color=YELLOW, weight=ft.FontWeight.BOLD, size=30)
-                                    ],
-                                    spacing=0,
-                                    alignment=ft.MainAxisAlignment.CENTER
-                    
+                                [
+                                    ft.Text(value="TIME", color=BLUE,
+                                            weight=ft.FontWeight.BOLD, size=30),
+                                    ft.Text(value="WISE", color=YELLOW,
+                                            weight=ft.FontWeight.BOLD, size=30)
+                                ],
+                                spacing=0,
+                                alignment=ft.MainAxisAlignment.CENTER
+
                             ),
-                            ft.Text(value="Bem vindo, usuário!", color=DARK_BLUE, size=20, weight=ft.FontWeight.BOLD)
+                            ft.Text(value="Bem vindo, usuário!", color=DARK_BLUE,
+                                    size=20, weight=ft.FontWeight.BOLD)
                         ],
                         alignment=ft.MainAxisAlignment.CENTER
                     ),
                     padding=ft.padding.symmetric(horizontal=8)
                 )
+            ),
+
+            ft.Container(
+                content=ft.Text("Semana: 13/01-17/01", color=DARK_BLUE,
+                                size=30, weight=ft.FontWeight.BOLD),
+                padding=ft.padding.symmetric(horizontal=30, vertical=10),
+                border_radius=16,
+                bgcolor=GREY
+            ),
+
+            ft.Row(
+                [
+                    ft.Column(horarioAleatorio("SEG"),
+                              alignment=ft.MainAxisAlignment.CENTER),
+                    ft.Column(horarioAleatorio("TER"),
+                              alignment=ft.MainAxisAlignment.CENTER),
+                    ft.Column(horarioAleatorio("QUA"),
+                              alignment=ft.MainAxisAlignment.CENTER),
+                    ft.Column(horarioAleatorio("QUI"),
+                              alignment=ft.MainAxisAlignment.CENTER),
+                    ft.Column(horarioAleatorio("SEX"),
+                              alignment=ft.MainAxisAlignment.CENTER)
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+                vertical_alignment=ft.CrossAxisAlignment.START
             )
         ]
     )
@@ -203,17 +268,18 @@ def main(page: ft.Page):
     def route_change(route):
         page.views.clear()
         page.views.append(login)
-        if page.route=="main":
+        if page.route == "main":
             page.views.append(main)
         page.update()
 
     def view_pop(view):
         page.views.pop()
-        top_view=page.views[-1]
+        top_view = page.views[-1]
         page.go(top_view.route)
 
-    page.on_route_change=route_change
-    page.on_view_pop=view_pop
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
     page.go(page.route)
 
-ft.app(main)
+
+ft.app(main, assets_dir="assets")
